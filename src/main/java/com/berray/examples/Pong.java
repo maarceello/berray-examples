9cfc6efb0dbbb5ef01ae2c6f5fc63aab581ddf08
Marcel Radhouani <marcel.radhouani@eisberg.cloud>
2024-07-17 20:41:06 +0200

    added example stuff

--- src/main/java/com/berray/examples/Pong.java
+++ src/main/java/com/berray/examples/Pong.java
@@ -3,12 +3,17 @@ package com.berray.examples;
 import com.berray.BerrayApplication;
 import com.berray.GameObject;
 
+import static com.raylib.Jaylib.RED;
+
 
 import static com.berray.AssetManager.loadSprite;
 import static com.berray.AssetManager.loadMusic;
 import static com.berray.components.PosComponent.pos;
+import static com.berray.components.RectComponent.rect;
+import static com.berray.components.CircleComponent.circle;
 import static com.berray.components.SpriteComponent.sprite;
 import static com.berray.components.RotateComponent.rotate;
+import static com.berray.components.TextComponent.text;
 
 public class Pong extends BerrayApplication {
 
@@ -16,25 +21,59 @@ public class Pong extends BerrayApplication {
   public void initWindow() {
     width(1024);
     height(768);
-    title("This is a Window");
+    background(RED);
+    title("Pong Game");
   }
 
   @Override
   public void initGame() {
 
-    loadSprite("bean","resources/berry.png");
+    loadSprite("berry", "resources/berry.png");
     loadMusic("wind", "resources/wind.mp3");
 
-    GameObject player = add(
-        sprite("bean")
-            .anim("idle")
-        ,
-        pos(129, 83),
-        rotate(89)
+
+    GameObject infoTxt = add(
+        text("Hello im a berry")
+    );
+
+    GameObject startTxt = add(
+        text("Press Space to start...not"),
+        pos(1024 / 2, 768 / 2)
+    );
+
+    GameObject rect = add(
+        rect(20, 80),
+        pos(129, 83)
+    );
+
+    GameObject rect2 = add(
+        rect(20, 80),
+        pos(300, 300),
+        rotate(45)
+    );
+
+    GameObject rect3 = add(
+        rect(80, 80),
+        pos(40, 768 - 40)
+    );
+
+    GameObject circle = add(
+        circle(80),
+        pos(400, 183)
+    );
+
+    GameObject berry = add(
+        sprite("berry"),
+        pos(110, 100),
+        rotate(45)
+    );
+
+    GameObject berry2 = add(
+        sprite("berry"),
+        pos(500, 100)
     );
 
   }
-
 
 
   public static void main(String[] args) {
