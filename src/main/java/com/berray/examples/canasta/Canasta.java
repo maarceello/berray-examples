6249118ac4c919a11ba654755330564b22c87d5c
marfsama <7756076+marfsama@users.noreply.github.com>
2025-08-30 11:46:26 +0200

    add arc based hand display, wip

--- src/main/java/com/berray/examples/canasta/Canasta.java
+++ src/main/java/com/berray/examples/canasta/Canasta.java
@@ -1,7 +1,10 @@
 package com.berray.examples.canasta;
 
 
-import com.berray.*;
+import com.berray.AnimationData;
+import com.berray.BerrayApplication;
+import com.berray.Game;
+import com.berray.GameObject;
 import com.berray.assets.CoreAssetShortcuts;
 import com.berray.assets.SpriteSheet;
 import com.berray.components.CoreComponentShortcuts;
@@ -16,9 +19,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static com.berray.examples.canasta.CardStackComponent.cardStack;
 import static com.berray.examples.canasta.DragComponent.draggable;
-import static com.berray.examples.canasta.DropTargetComponent.dropTarget;
+import static com.berray.math.MathUtil.*;
 
 public class Canasta extends BerrayApplication implements CoreComponentShortcuts, CoreAssetShortcuts, CoreEvents {
 
@@ -28,7 +30,7 @@ public class Canasta extends BerrayApplication implements CoreComponentShortcuts
     public void initWindow() {
         width(1500);
         height(768);
-        background(Color.GRAY);
+        background(new Color(0,255.0f/100,0));
         title("Canasta Card Game");
     }
 
@@ -51,9 +53,12 @@ public class Canasta extends BerrayApplication implements CoreComponentShortcuts
 
         CardStack stack = new CardStack("Ablagestapel");
 
+
+
         for (int x = 0; x < 14; x++) {
             Card card = deck.get(x);
             stack.addCard(card);
+
             int frame = card.getCardSuit().ordinal() * 14 + card.getCardValue().ordinal() + 1;
 
             GameObject cardObject = add(
@@ -77,18 +82,96 @@ public class Canasta extends BerrayApplication implements CoreComponentShortcuts
                     scale(1.0f)
 //                    new FlipComponent(frame, 3*14)
             );
-//            cardObject.on(CoreEvents.HOVER_ENTER, this::onHoverEnter);
-//            cardObject.on(CoreEvents.HOVER_LEAVE, this::onHoverLeave);
         }
 
-        GameObject cardStack = add(
-            pos(500, 100),
-            cardStack(stack),
-            area(),
-            mouse(),
-            draggable(),
-            dropTarget("card")
+//        add(
+//            pos(530, 240),
+//            cardStack(stack),
+//            area(),
+//            mouse(),
+//            draggable(),
+//            dropTarget("card")
+//        );
+
+        // Draw the arc of cards at the bottom of the screen
+        int screenCenter = width() / 2;
+        int arcRadius = 300;
+        float arcAngle = 90; // degrees
+
+        drawCardArc(stack.getCards(), screenCenter, height()/2, arcRadius, arcAngle);
+    }
+
+    // Function to draw cards in an arc
+    private List<GameObject> drawCardArc(List<Card> cards, int x, int y, int radius, float totalAngle) {
+        float angleStep = toRadians(cards.isEmpty() ? 1 : totalAngle / (cards.size()));
+        float startAngle = toRadians(-totalAngle / 2);
+
+        List<GameObject> cardObjects = new ArrayList<>();
+
+        add(
+                pos(x,y),
+                circle(radius),
+                color(Color.GOLD),
+                anchor(AnchorType.CENTER)
         );
+
+        float zeroAngle = (float) (Math.PI / 2 + Math.PI);
+        float start = zeroAngle - toRadians(totalAngle/2);
+
+
+        for (int i = 0; i < cards.size(); i++) {
+            float angle = startAngle + (i * angleStep);
+            float xPos = x + radius * sin(angle);
+            float yPos = y + radius * cos(angle);
+
+            add(
+                    pos(x + cos(start + angleStep * i) * radius, y + sin(start + angleStep * i) * radius),
+                    circle(10),
+                    color(Color.GREEN),
+                    anchor(AnchorType.CENTER)
+            );
+
+
+            Card card = cards.get(i);
+            int frame = card.getCardSuit().ordinal() * 14 + card.getCardValue().ordinal() + 1;
+
+            // Create a card object
+            GameObject cardObj = add(
+                    pos(xPos, yPos),
+                    area(),
+                    rotate(angle),
+                    anchor(AnchorType.CENTER),
+                    sprite("cards").frame(frame),
+                    z(i), // Ensure cards overlap correctly
+                    property("originalPos", new Vec2(xPos, yPos)),
+                    property("isSelected", false)
+            );
+
+
+//            // Make cards interactive
+//            cardObj.onClick(() => {
+//                    cardObj.isSelected = !cardObj.isSelected;
+//            if (cardObj.isSelected) {
+//                cardObj.moveTo(cardObj.originalPos.x, cardObj.originalPos.y - 30);
+//                cardObj.color = rgb(200, 200, 255);
+//            } else {
+//                cardObj.moveTo(cardObj.originalPos);
+//                cardObj.color = rgb(255, 255, 255);
+//            }
+//    });
+
+            cardObj.on(HOVER_ENTER,  (event) -> {
+                    cardObj.set("scale", new Vec2(1.05f, 1.05f));
+            });
+
+            cardObj.on(HOVER_LEAVE, (event) -> {
+                cardObj.set("scale", new Vec2(1.0f, 1.0f));
+            });
+
+            cardObjects.add(cardObj);
+        }
+
+        return cardObjects;
     }
 
     private void onHoverEnter(Event event) {
